be66bf3250aa9228c26847a30447d73f700275cc
OliverStealer <54906997+OliverStealer@users.noreply.github.com>
2020-05-10 01:36:56 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,14 +3,14 @@ const bot = new Discord.Client();
 bot.on('ready', () => {
     console.log('I am ready!');
 });
-bot.on('message', message => {
+/*bot.on('message', message => {
     if (message.content === 's/ping') {
         var pingembed = new Discord.RichEmbed()
         .setColor("#4682B4")
         .setTitle(`Ping ${bot.ping}ms pong :ping_pong:`)
          message.channel.send(pingembed).then(msg => msg.delete(600000));
       }
-});
+});*/
 
 
 bot.commands = new Discord.Collection();
