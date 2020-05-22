c9cc12b8ce34e78faec50fa7106d051ec1a75b6f
OliverStealer <54906997+OliverStealer@users.noreply.github.com>
2020-05-23 00:12:17 +0500

    Update avatar.js

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
 
     if(message.content.toLowerCase().startsWith("s/avatar"))
+    message.delete('s/avatar')
     {
     let mb = message.mentions.members.first() || message.member; // Если есть упомянание человека в сообщении, то берём его, если нету, то себя. Расскажу чуть позже.
     let color = mb.displayHexColor; //Цвет самой высокой роли человека, если цвет невидимый то самой высокой отображаемой роли.
@@ -10,8 +11,8 @@ module.exports.run = async (bot,message,args) => {
     let embed = new Discord.RichEmbed() //Создаём эмбед
     .setImage(mb.user.avatarURL) //Устанавливаем картинку - аватар человека.
     .setColor(color) //Цвет.
-    .setFooter("Аватар пользователя " + mb.user.tag); //Устанавливаем в подпись чей это аватар.
-    message.channel.send({embed}).then(msg => msg.delete(600000)); //Отправляем.
+    .setFooter("**Аватар пользователя**" + mb.user.tag); //Устанавливаем в подпись чей это аватар.
+    message.reply({embed}).then(msg => msg.delete(600000)); //Отправляем.
   }
 };
 
