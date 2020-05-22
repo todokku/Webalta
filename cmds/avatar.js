0c045325cefff4e31f0210a220f68f78fa98efeb
OliverStealer <54906997+OliverStealer@users.noreply.github.com>
2020-05-23 00:13:58 +0500

    Update avatar.js

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -5,14 +5,14 @@ module.exports.run = async (bot,message,args) => {
     if(message.content.toLowerCase().startsWith("s/avatar"))
     message.delete('s/avatar')
     {
-    let mb = message.mentions.members.first() || message.member; // Если есть упомянание человека в сообщении, то берём его, если нету, то себя. Расскажу чуть позже.
-    let color = mb.displayHexColor; //Цвет самой высокой роли человека, если цвет невидимый то самой высокой отображаемой роли.
-    if (color == '#000000') color = mb.hoistRole.hexColor;//Цвет самой высокой роли человека.
-    let embed = new Discord.RichEmbed() //Создаём эмбед
-    .setImage(mb.user.avatarURL) //Устанавливаем картинку - аватар человека.
-    .setColor(color) //Цвет.
-    .setFooter("**Аватар пользователя**" + mb.user.tag); //Устанавливаем в подпись чей это аватар.
-    message.reply({embed}).then(msg => msg.delete(600000)); //Отправляем.
+    let mb = message.mentions.members.first() || message.member;
+    let color = mb.displayHexColor;
+    if (color == '#000000') color = mb.hoistRole.hexColor;
+    let embed = new Discord.RichEmbed()
+    .setImage(mb.user.avatarURL)
+    .setColor(color)
+    .setFooter("**Аватар пользователя**" + mb.user.tag);
+    message.reply({embed}).then(msg => msg.delete(600000));
   }
 };
 
