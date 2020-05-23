a5bc04c290ff5879ef683b8195ffc39aed5ab334
OliverStealer <54906997+OliverStealer@users.noreply.github.com>
2020-05-24 02:15:48 +0500

    Create reload.js

---
+++ cmds/reload.js
@@ -0,0 +1,20 @@
+module.exports.run = async (Discord, bot, message, args) => {
+    if (message.author.id != "520295470266843157") return message.channel.send("Ты не мой хозяин((")
+    if (!args[0]) return message.channel.send("А де название команды?")
+   
+    let commandName = args[0].toLowerCase()
+    try {
+        delete require.cache[require.resolve(`./${commandName}.js`)]
+        bot.commands.delete(commandName)
+        const pull = require(`./${commandName}.js`)
+        bot.commands.set(commandName, pull)
+    } catch (e) {
+        return message.channel.send(` ${args[0].toLowerCase()} не могу найти данную команду`)
+
+    }
+    message.channel.send(` ``${args[0].toUpperCase()}.js`` кастом`)
+}
+module.exports.help = {
+    name: 'reload',
+    aliases: []
+}
