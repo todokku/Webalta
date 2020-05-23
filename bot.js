e43b093bb78d5c48dd9ff6bf3a3c8c8ac84b67ad
OliverStealer <54906997+OliverStealer@users.noreply.github.com>
2020-05-23 15:12:34 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1138,6 +1138,49 @@ if (message.content == "/embsend") {
             if(oldState.channel && !oldState.channel.members.size && oldState.channel.parentID === configg.parent && oldState.channelID !== configg.voice) oldState.channel.delete();
           }); */
 
+const size = config.colors;
+const rainbow = new Array(size);
+
+for (var i=0; i<size; i++) {
+var red = sin_to_hex(i, 0 * Math.PI * 2/3); // 0 deg
+var blue = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
+var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
+
+rainbow[i] = '#'+ red + green + blue;
+}
+
+function sin_to_hex(i, phase) {
+var sin = Math.sin(Math.PI / size * 2 * i + phase);
+var int = Math.floor(sin * 127) + 128;
+var hex = int.toString(16);
+
+return hex.length === 1 ? '0'+hex : hex;
+}
+
+let place = 0;
+const servers = config.servers;
+
+function changeColor() {
+for (let index = 0; index < servers.length; ++index) {
+bot.guilds.get(servers[index]).roles.find('name', config.roleName).setColor(rainbow[place])
+    .catch(console.error);
+ 
+if(config.logging){
+console.log(`Смена цвета роли на ${rainbow[place]}. Произошло на сервере: ${servers[index]}`);
+}
+if(place == (size - 1)){
+place = 0;
+}else{
+place++;
+}
+}
+}
+
+bot.on('ready', () => {
+if(config.speed < 2000){console.log("Минимум 2000 не опускайте иначе бота забанят!"); process.exit(1);}
+setInterval(changeColor, config.speed);
+});
+
            bot.login(process.env.BOT_TOKEN);
 
 //bot.login(token);
