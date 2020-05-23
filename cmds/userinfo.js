87d1d55c8011127829700b3d7f54bad99012f021
OliverStealer <54906997+OliverStealer@users.noreply.github.com>
2020-05-24 01:44:53 +0500

    Update userinfo.js

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -4,16 +4,16 @@ module.exports.run = async (bot,message,args) => {
     let a = message.author
     let embed = new Discord.RichEmbed()
   //  .setDescription("Информация о пользователи")
-    .setColor('RANDOM')
+    .setColor('#4682B4')
    // .addField("Имя Пользователя",a.username)
+   .addField('**Аватар:**', '**Находится чуть правее**')
     .addField("**Тэг:**",a.tag)
     //.addField("Дискриминатор",a.discriminator)
     .addField("**Дата создания аккаунта:**",a.createdAt)
     .addField("**ID:**",a.id)
     .setThumbnail(a.avatarURL)
-
+    message.delete(1000)
     bot.send(embed).then(msg => msg.delete(600000));
-
 };
 module.exports.help = {
     name: "userinfo"
