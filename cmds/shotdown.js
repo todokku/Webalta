4982501e0e6c82b06b7b0afe51690c6acd6b0f82
OliverStealer <54906997+OliverStealer@users.noreply.github.com>
2020-05-26 12:30:17 +0500

    Update shotdown.js

--- cmds/shotdown.js
+++ cmds/shotdown.js
@@ -1,13 +1,18 @@
-module.exports.run = async (Discord, bot, message, args) => {
+const Discord = module.require("discord.js");
+const fs = require("fs");
+
+module.exports.run = async (bot,message,args) => {
     if(message.author.id != "492256216374837249") return message.channel.send("Ты не мой хозяин((")
- try {
-     await message.channel.send("Я спать")
-     process.exit()
- } catch(e) {
-     message.channel.send(`ERROR: ${e.message}`)
- }
-}
+
+    try {
+        await message.channel.send("Я спать")
+        process.exit()
+    } catch(e) {
+        message.channel.send(`ERROR: ${e.message}`)
+    }
+    
+    }
+
 module.exports.help = {
-    name: 'shotdown',
-    aliases: []
-}
+    name: "shutdown"
+};
