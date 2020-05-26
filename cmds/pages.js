bd0d673f7e4863959bb76a0687d873966bab18d2
OliverStealer <54906997+OliverStealer@users.noreply.github.com>
2020-05-27 02:11:24 +0500

    Update pages.js

--- cmds/pages.js
+++ cmds/pages.js
@@ -1,3 +1,4 @@
+/*
 const Discord = require('discord.js'); 
  exports.run = (bot,message,args) => {
   let pages = []; 
@@ -29,7 +30,7 @@ const Discord = require('discord.js');
       });
       forwards.on('collect', r => { 
         if (page === pages.length) return; 
-        page++; 
+        page--; 
         embed.setDescription(pages[page-1])
         .addField('**Команды модерации**', '`s/warn` **┃ выдать предупреждение**\n`s/unwarn` **┃ снять предупреждение**\n`s/ban` **┃ выдать блокировку**\n`s/mute` **┃ выдать мут**')
         msg.edit(embed) 
@@ -38,6 +39,45 @@ const Discord = require('discord.js');
   })
 }
 
+module.exports.help = {
+    name: "pages",
+    aliases: []
+};
+*/
+
+const Discord = require('discord.js'); 
+ 
+exports.run = (bot,message,args) => {
+  let pages = []; 
+  let page = 1; 
+  const embed = new Discord.RichEmbed() 
+    .setColor(0xffffff)
+    .setFooter(`Page ${page} of ${pages.length}`) 
+    .setDescription(pages[page-1])
+  message.channel.send(embed).then(msg => { 
+    msg.react('⏪').then( r => { 
+      msg.react('⏩') 
+      const backwardsFilter = (reaction, user) => reaction.emoji.name === '⏪' && user.id === message.author.id;
+      const forwardsFilter = (reaction, user) => reaction.emoji.name === '⏩' && user.id === message.author.id;
+      const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 }); 
+      const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 });
+      backwards.on('collect', r => { 
+        if (page === 1) return; 
+        page--; 
+        embed.setDescription(pages[page-1]); 
+        embed.setFooter(`Page ${page} of ${pages.length}`); 
+        msg.edit(embed) 
+      })
+      forwards.on('collect', r => { 
+        if (page === pages.length) return; 
+        page++; 
+        embed.setDescription(pages[page-1]); 
+        embed.setFooter(`Page ${page} of ${pages.length}`); 
+        msg.edit(embed) 
+      }) 
+    })
+  })
+}
 module.exports.help = {
     name: "pages",
     aliases: []
