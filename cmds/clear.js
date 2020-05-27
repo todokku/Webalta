252a09de0c51a8dfbd15154eadb8c0bbef2089af
OliverStealer <54906997+OliverStealer@users.noreply.github.com>
2020-05-28 01:12:14 +0500

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -1,3 +1,4 @@
+/*
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
@@ -19,3 +20,87 @@ module.exports.run = async (bot,message,args) => {
 module.exports.help = {
     name: "clear"
 };
+*/
+
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = function(bot, message, args) {
+    //if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("❌**Error:** You don't have the **Manage Messages** permission!");
+    if(!args[0]) return message.reply('`Используй: purge all|bots|user|author|images <кол-во>` <:err:715285004657229896>')
+    if(args[0] === 'all') {
+      if(!args[1]) return message.channel.send('`Нужно указать количество` <:err:715285004657229896>');
+      if(isNaN(args[1])) return message.channel.send('`Укажи действительное количество` <:err:715285004657229896>');
+      if(parseInt(args[1]) > 100) return message.channel.send('`Я могу удалить максимум 100 сообщений за раз` <:err:715285004657229896>')
+  
+      let messagecount = parseInt(args[1]);
+      message.channel.messages.fetch({
+        limit: 100
+      }).then(messages => message.channel.bulkDelete(messagecount))
+      .catch(e => {
+        if(e) return message.channel.send("Error: ", e)
+      })
+    }
+    else if(args[0] === 'bots') {
+      if(!args[1]) return message.channel.send('`Нужно указать количество` <:err:715285004657229896>');
+      if(isNaN(args[1])) return message.channel.send('`Укажи действительное количество` <:err:715285004657229896>');
+      if(parseInt(args[1]) > 100) return message.channel.send('`Я могу удалить максимум 100 сообщений за раз` <:err:715285004657229896>')
+  
+      message.channel.messages.fetch({
+        limit: args[1]
+      }).then(messages => {
+        const userMessages = messages.filter(message => message.author.bot) 
+        message.channel.bulkDelete(userMessages)
+      }).catch(e => {
+        if(e) return message.channel.send("Error: ", e)
+      })
+    }
+    else if(args[0] === 'user') {
+      if(!args[1]) return message.channel.send('`Нужно указать количество` <:err:715285004657229896>');
+      if(isNaN(args[1])) return message.channel.send('`Укажи действительное количество` <:err:715285004657229896>');
+      if(parseInt(args[1]) > 100) return message.channel.send('`Я могу удалить максимум 100 сообщений за раз` <:err:715285004657229896>')
+  
+      message.channel.messages.fetch({
+        limit: args[1]
+      }).then(messages => {
+        const userMessages = messages.filter(message => !message.author.bot) 
+        message.channel.bulkDelete(userMessages)
+      }).catch(e => {
+        if(e) return message.channel.send("Error: ", e)
+      })
+    }
+    else if(args[0] === 'author'){
+      if(!message.mention || message.mentions.users.size < 1) return message.channel.send("Ping someone to delete their message!")
+      if(!args[2]) return message.channel.send('`Нужно указать количество` <:err:715285004657229896>');
+      if(isNaN(args[2])) return message.channel.send('`Укажи действительное количество` <:err:715285004657229896>');
+      if(parseInt(args[2]) > 100) return message.channel.send('`Я могу удалить максимум 100 сообщений за раз` <:err:715285004657229896>')
+  
+      message.channel.messages.fetch({
+        limit: parseInt(args[2])
+      }).then(messages => {
+        const userMessages = messages.filter(message => message.mentions.users.first() || message.author) 
+        message.channel.bulkDelete(userMessages)
+      }).catch(e => {
+        if(e) return message.channel.send("<:err:715285004657229896> ", e)
+      })
+    }
+    else if(args[0] === 'image') {
+      message.reply("Новые возможности")
+    }
+    else {
+      message.reply('Используй: purge all|bots|user|author <кол-во>')
+    }
+  };
+  
+  module.exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 1
+  };
+  
+  module.exports.help = {
+    name: 'clear',
+    description: 'Purges X amount of messages from a given channel.',
+    usage: 'purge all|bots|user|author <amount>'
+  };
+  
