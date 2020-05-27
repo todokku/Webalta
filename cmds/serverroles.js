76cb352ab715a346df7df20f52745b37c07fc8e8
OliverStealer <54906997+OliverStealer@users.noreply.github.com>
2020-05-27 21:35:40 +0500

    Update serverroles.js

--- cmds/serverroles.js
+++ cmds/serverroles.js
@@ -3,7 +3,8 @@ module.exports.run = async (bot, message, args) => {
 
     let roles = new Discord.RichEmbed()
     .setColor(`#4682B4`)
-    .addField(`**Ролей на сервере**`, `${message.guild.roles.size}\nИмя ролей: ${message.guild.roles.array()}`, true)
+    .setDescription(`Ролей на сервере: ${message.guild.roles.size}`)
+    .addField(`**Имя ролей:**`, `${message.guild.roles.array()}`, true)
     .setFooter(`${message.author.tag}`, `${message.author.displayAvatarURL}`)
 
     message.channel.send(roles);
