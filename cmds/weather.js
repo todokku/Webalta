51c182636b1a3e4cadd7c72f22251852697d03cd
OliverStealer <54906997+OliverStealer@users.noreply.github.com>
2020-05-27 22:09:34 +0500

    Update weather.js

--- cmds/weather.js
+++ cmds/weather.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const config = require('../config.json');
 const weather = require("weather-js")
-exports.run = async (client, message, args) => {
+exports.run = async (bot, message, args) => {
  weather.find({search: encodeURIComponent(args.join(" ")), degreeType: "C"}, function(err, result) {
     if(!result) return message.channel.send("Город не найден").then(console.error(err));
  if(!args[0]) return message.channel.send(`\`\`\`ERR no args\`\`\``)
@@ -17,7 +17,7 @@ exports.run = async (client, message, args) => {
             .setDescription(`**${current.skytext}**`)
             .setAuthor(`Weather of ${current.observationpoint}`)
             .setThumbnail(`${current.imageURL}`)
-            .setColor("#4682B4")
+            .setColor("RANDOM")
             .addField("Часовой пояс", `UTC${current.observationpoint}`, true)
             .addField("Тип",location.degreetype, true)
             .addField("Температура", `${current.temperature} Degrees`, true)
@@ -34,8 +34,6 @@ exports.conf = {
   aliases: ['w']
 };
 
-exports.help = {
-  name: 'weather',
-  category: 'info',
-  desc:"Погода"
-}
+module.exports.help = {
+  name: "weather"
+};
