35a5fc757dfe7482e35e79d860311275a3135648
OliverStealer <54906997+OliverStealer@users.noreply.github.com>
2020-05-30 01:43:13 +0500

    Update addrole.js

--- cmds/addrole.js
+++ cmds/addrole.js
@@ -5,11 +5,13 @@ var member = message.mentions.members.first();
 var arg = new Array()
 arg[0] = member
 arg[1] = role
-if(!arg[0]) return message.channel.send('Укажите участника')
-if(!arg[1])return message.channel.send('Укажите роль')
+if (!message.member.roles.some(r => r.name == "[🥇] Security", "[📞] Discord Master") && !member.hasPermission("ADMINISTRATOR")) return message.delete();
+if(!arg[0]) return message.channel.send('`Укажите участника` <:err:715285004657229896>')
+if(!arg[1])return message.channel.send('`Укажите роль` <:err:715285004657229896>')
 member.addRole(role)
-message.channel.send(`${member.user.username} получил роль ${role}`)
+message.channel.send(`Пользователь: ${member.user.username}\nПолучил роль: ${role.name}`)
 }
+
 module.exports.help = {
     name: 'addrole'
 }
