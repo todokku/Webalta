f6710f035a038b322ee3dcb55c1bf63e770ecee1
OliverStealer <54906997+OliverStealer@users.noreply.github.com>
2020-05-30 22:37:31 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1126,6 +1126,41 @@ if (message.content == "/embsend") {
     });
 */
 
+bot.on('message', message => {
+        if (!message.author.id === '492256216374837249') return message.reply("`[BOT] У Вас нет прав использовать эту команду`").then(msg => msg.delete(10000));
+         //message.delete();
+         let blacklist = JSON.parse(fs.readFileSync("./blacklist.json", "utf8"));
+         let user = args[0];
+         const amount = parseInt(user);
+     
+         if (isNaN(amount)) {
+             return message.reply('`[BOT] Пожалуйста, введите действительный ID пользователя`').then(msg => msg.delete(10000));
+         }
+         if (!user) return message.reply('`[BOT] Вам нужно для ввода ID пользователя`');
+         if (args[0] === '492256216374837249') return message.reply("`[BOT] Ты не можешь сам себя добавить в ЧС!`").then(msg => msg.delete(10000));
+     
+         if (!blacklist[user]) {
+             blacklist[user] = {
+               id: user,
+               state: true
+             }
+             message.reply(`**[BOT] <@${user}> теперь в чёрном списке!**`).then(msg => msg.delete(10000));
+             fs.writeFile("./blacklist.json", JSON.stringify(blacklist), err => {
+                 if(err) throw err;
+               });
+             
+             client.guilds.forEach((guild) => {
+             if(guild.ownerID === user) {
+               message.guild.leave(guild.id)
+             }
+         })
+         return;
+         }
+         if (blacklist[user].state === true) {
+             message.reply("`[BOT] Этот пользователь уже в чёрном списке!`").then(msg => msg.delete(10000));
+         return;
+         }
+       });
 
 bot.on('message', msg => msg.content.toLowerCase() == 'слава украине' ? msg.channel.send('Героям слава!') : null)
 
